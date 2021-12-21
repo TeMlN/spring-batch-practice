b08161c8308d07cf1b89661bece4d2cf877c2762
taem1n1 <youman7953@naver.com>
2021-12-21 11:48:48 +0900

    Initial Commit

---
+++ src/main/java/com/example/springbatch/config/ExampleJobConfig.java
@@ -0,0 +1,10 @@
+package com.example.springbatch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@Enabl
+public class ExampleJobConfig {
+}
