76945789817a7cf09978fadf14ae5b6cd6eae932
taem1n1 <youman7953@naver.com>
2022-03-17 20:08:39 +0900

    rename

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbatchApplication {
+public class SpringBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbatchApplication.class, args);
+		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
 }
